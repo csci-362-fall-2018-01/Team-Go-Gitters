2708dd8b01d4df0fb84ca4b9f2a65c072e77185c
EyYoTony <aswanson076@gmail.com>
2018-09-23 19:55:47 -0400

    added basic myList bash script

---
+++ myList.sh
@@ -0,0 +1,6 @@
+#Team Go Gitters - Jacob Mattox, Kyle Cooper, Lara Brooksbank, Alexander Swanson
+
+#Gets files in current directory and pipes them to an html file called myList.html
+ls > myList.html
+
+#To run this make sure it is an executable 'chmod -x myList.sh' then run it with `./myList.sh`
