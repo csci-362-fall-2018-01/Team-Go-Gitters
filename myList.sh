f71417c73f1623f0b37dcb3dcdd81892760d4b74
Jacob <mattoxjr@g.cofc.edu>
2018-09-24 12:54:58 -0400

    Added another option using for loop (commented out)

--- myList.sh
+++ myList.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 #Team Go Gitters - Jacob Mattox, Kyle Cooper, Lara Brooksbank, Alexander Swanson
 
 #Gets files in current directory and pipes them to an html file called myList.html
 ls > myList.html
 
-#To run this make sure it is an executable 'chmod -x myList.sh' then run it with `./myList.sh`
+#To run this make sure it is an executable 'chmod +x myList.sh' then run it with `./myList.sh`
+
+#Another possible way to do this with a loop is as follows (requires name of directory):
+#cd..
+#for file in Team-Go-Gitter/*; do
+	#echo $file >> outlist.html;
+	#done
