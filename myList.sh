a2cb32dbb8d3ca52303e49eed15525f71179209a
EyYoTony <aswanson076@gmail.com>
2018-09-24 13:47:55 -0400

    made myList iterative so no just text

--- myList.sh
+++ myList.sh
@@ -2,12 +2,15 @@
 #Team Go Gitters - Jacob Mattox, Kyle Cooper, Lara Brooksbank, Alexander Swanson
 
 #Gets files in current directory and pipes them to an html file called myList.html
-ls > myList.html
+#ls > myList.html
 
 #To run this make sure it is an executable 'chmod +x myList.sh' then run it with `./myList.sh`
 
 #Another possible way to do this with a loop is as follows (requires name of directory):
-#cd..
-#for file in Team-Go-Gitter/*; do
-	#echo $file >> outlist.html;
-	#done
+#clear the .html file
+> outlist.html
+#iterate through the directory
+for file in *; do
+	echo $file >> outlist.html;
+  echo "<br/>" >> outlist.html;
+	done
