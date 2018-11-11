50060a56622b1cc3676e02704f7b682f2532f642
Jacob <mattoxjr@g.cofc.edu>
2018-11-11 18:41:43 -0500

    added all test files

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -11,3 +11,49 @@ filepath=$(readlink -f "$filepath")
 sh ./scripts/test1.sh
 
 sh ./scripts/test2.sh
+
+sh ./scripts/test3.sh
+
+sh ./scripts/test4.sh
+
+sh ./scripts/test5.sh
+
+sh ./scripts/test6.sh
+
+sh ./scripts/test7.sh
+
+sh ./scripts/test8.sh
+
+sh ./scripts/test9.sh
+
+sh ./scripts/test10.sh
+
+sh ./scripts/test11.sh
+
+sh ./scripts/test12.sh
+
+sh ./scripts/test13.sh
+
+sh ./scripts/test14.sh
+
+sh ./scripts/test15.sh
+
+sh ./scripts/test16.sh
+
+sh ./scripts/test17.sh
+
+sh ./scripts/test18.sh
+
+sh ./scripts/test19.sh
+
+sh ./scripts/test20.sh
+
+sh ./scripts/test21.sh
+
+sh ./scripts/test22.sh
+
+sh ./scripts/test23.sh
+
+sh ./scripts/test24.sh
+
+sh ./scripts/test25.sh
