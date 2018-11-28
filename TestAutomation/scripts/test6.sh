3669827debe4223f8d7bcbfc5c86cb27fbfa0e66
cooperkm <cooperkm@g.cofc.edu>
2018-11-28 14:19:12 -0500

    test6.sh
    Got mathFunction 1 to run

--- TestAutomation/scripts/test6.sh
+++ TestAutomation/scripts/test6.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+cd ./scripts        #my  runAllTests.sh is located in ./TestAutomation and therefore needs to cd into where test_MathFunction1.cpp is located
 export PATH=$PATH:~/Desktop/Celestia/Celestia
 echo "STARTING TEST 6. . ."
-sudo make ./scripts/test_MathFunction4
-sudo ./scripts/test_MathFunction4
+
+g++ test_MathFunction1.cpp -o test_MathFunction1  #compiles the executable
+
+./test_MathFunction1   #Executes test. file name drops the .cpp extension after being compiled
+
+echo "TEST 6 ENDED. . ."
+
+#sudo make ./scripts/test_MathFunction4
+#sudo ./scripts/test_MathFunction4
