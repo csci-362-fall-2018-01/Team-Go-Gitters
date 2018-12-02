327e169721b022fd658dd7a4c8c0be7eb500924d
Jacob <mattoxjr@g.cofc.edu>
2018-12-02 14:20:17 -0500

    updated test 1/2 to try and install new files for celestia

--- TestAutomation/scripts/test1.sh
+++ TestAutomation/scripts/test1.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+filepath=$(dirname "$f")
+filepath=$(readlink -f "$filepath")
 
 
 export PATH=$PATH:Celestia/
 echo "<div><h3>TEST 1</h3>" >> temp/test1log.txt
 echo "$(date) Test 1: Starting" >> temp/test1log.txt
 echo "<br/>" >> temp/test1log.txt
+cd Celestia/
 sudo apt-get install freeglut3-dev libjpeg-dev libpng-dev build-essential automake libgtkglext1-dev libreadline-dev liblua5.1-0-dev liblua50-dev liblua50-dev liblualib50-dev
 sudo apt-get update
 sudo apt-get install autoconf libtool-bin
-echo "$(date): All packages successfully installed or already found in system." >> temp/test1log.txt
-echo "<br/>" >> temp/test1log.txt
+echo "$(date): All packages successfully installed or already found in system." >> $filepath/temp/test1log.txt
+echo "<br/>" >> $filepath/temp/test1log.txt
 
-echo "$(date) Test 1: Finished $(\n)" >> temp/test1log.txt
-echo "<br/></div>" >> temp/test1log.txt
+echo "$(date) Test 1: Finished $(\n)" >> $filepath/temp/test1log.txt
+echo "<br/></div>" >> $filepath/temp/test1log.txt
+
+
+cd ..
