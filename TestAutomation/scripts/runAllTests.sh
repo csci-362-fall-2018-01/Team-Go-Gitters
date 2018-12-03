e46da06c8b2cacf356c65f3ab6e37148ff7fbbd4
EyYoTony <aswanson076@gmail.com>
2018-12-02 19:11:21 -0500

    runAllTests now iterates through test cases

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -10,36 +10,39 @@ filepath=$(readlink -f "$filepath")
 rm -f temp/*
 
 
-sh ./scripts/test1.sh
+ #sh ./scripts/test1.sh
 
-sh ./scripts/test2.sh
+ #sh ./scripts/test2.sh
 
-sh ./scripts/test3.sh
+ #sh ./scripts/test3.sh
 
-sh ./scripts/test4.sh
+ #sh ./scripts/test4.sh
 
-sh ./scripts/test5.sh
+ #sh ./scripts/test5.sh
 
- sh ./scripts/test6.sh
+ #sh ./scripts/test6.sh
 
- sh ./scripts/test7.sh
+ #sh ./scripts/test7.sh
 
- sh ./scripts/test8.sh
+ #sh ./scripts/test8.sh
 
- sh ./scripts/test9.sh
+ #sh ./scripts/test9.sh
 
- sh ./scripts/test10.sh
+ #sh ./scripts/test10.sh
 
- sh ./scripts/test11.sh
+ #sh ./scripts/test11.sh
 
-# sh ./scripts/test12.sh
+ #sh ./scripts/test12.sh
+
+for i in `seq 1 11`;
+do
+  sh ./scripts/test$i.sh
+done
 
 for line in $(ls -tr temp/);
 do
     cat "temp/$line" >> temp/outputFile.html
     echo '<br/>' >> temp/outputFile.html
-
 done
 
 xdg-open temp/outputFile.html
-
